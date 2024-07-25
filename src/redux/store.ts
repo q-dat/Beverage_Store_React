afe36e7b8414b45763e2b5733d0355fcd97c7959
q-dat <quocdastore79@gmail.com>
2024-07-25 11:30:05 +0700

    feat: add redux-form

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { productsApi } from "../services/admin/products.services";
-export const store = configureStore({
-  reducer: {
-    [productsApi.reducerPath]: productsApi.reducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(productsApi.middleware),
-});
+import { reducer as formReducer } from 'redux-form';
+// import { productsApi } from "../services/admin/products.services";
+// export const store = configureStore({
+//   reducer: {
+//     [productsApi.reducerPath]: productsApi.reducer,
+//   },
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware().concat(productsApi.middleware),
+// });
+
+const productsReducer = (state = [], _action: any) => {
+    return state;
+  };
+  
+  const usersReducer = (state = [], _action: any) => {
+    return state;
+  };
+  
+  export const store = configureStore({
+    reducer: {
+      products: productsReducer,
+      users: usersReducer,
+      form: formReducer,
+    },
+  });
+  
--- src/services/admin/products.services.ts
+++ src/services/admin/products.services.ts
@@ -1,23 +1,23 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { IProductResponse } from "../../types/Products";
+// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+// import { IProductResponse } from "../../types/Products";
 
-export const productsApi = createApi({
-  reducerPath: "listApi",
-  baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_API_URL,
-  }),
-  endpoints: (builder) => ({
-    getAllProducts: builder.query<IProductResponse, void>({
-      query: () => "/products",
-    }),
-    DeleteProducts: builder.mutation<void, string>({
-      query: (id) => ({
-        url: `/products/${id}`,
-        method: "DELETE",
-      }),
-    }),
-  }),
-});
+// export const productsApi = createApi({
+//   reducerPath: "listApi",
+//   baseQuery: fetchBaseQuery({
+//     baseUrl: import.meta.env.VITE_API_URL,
+//   }),
+//   endpoints: (builder) => ({
+//     getAllProducts: builder.query<IProductResponse, void>({
+//       query: () => "/products",
+//     }),
+//     DeleteProducts: builder.mutation<void, string>({
+//       query: (id) => ({
+//         url: `/products/${id}`,
+//         method: "DELETE",
+//       }),
+//     }),
+//   }),
+// });
 
-export const { useGetAllProductsQuery, useDeleteProductsMutation } =
-  productsApi;
+// export const { useGetAllProductsQuery, useDeleteProductsMutation } =
+//   productsApi;
